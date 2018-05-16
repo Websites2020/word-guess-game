92be322a4fac19452d7679b60d639a5a16cfc4b5
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-05-16 19:56:03 -0400

    third

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,62 @@
-var hero=["apple", "orange", "pear"];
-var guesses=3;
-var randomHero=[];
-var playerGuess=[];
-var noGuess=[];
-
-console.log(hero.length)
-
-//computer chooses random hero
-function randomHero() {
-    document.getElementById("myHero").innerHTML = [(Math.random() * hero.length)];
+function square(number) {
+    console.log(number * number);
 }
 
-// randomHero();
+square(3)
 
-console.log([(Math.random() * hero.length)])
+var x = 6
 
-//player guesses a letter
-function playerGuess() {
-    document.getElementById("player").onkeyup = function() {playerGuess()};
+var y = 1
+
+var computerChoices = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+
+    console.log(computerChoices.length)
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    console.log([Math.floor(Math.random() * computerChoices.length)]);
+
+
+    function playerWin() {
+        pWins++;
+    }
+    
+    function compWin() {
+        cWins++;
+    }
+
+
+    document.onkeyup = function(event) {
+        var userGuess = event.key;
+
+        console.log(userGuess);
+
+
+    if (userGuess == computerGuess) {
+
+        var winTally = document.getElementById("wins");
+        wins = wins+1;
+        winTally.innerHTML = "Player Wins: " + wins;    
+
+        console.log("true");
+
+    }  else {
+
+        var guessTally = document.getElementById("guess");
+        guess += userGuess;
+        guessTally.textContent = "Player Guesses: " + guess;
+
+        var leftTally = document.getElementById("left");
+        left = x--;
+        leftTally.innerHTML = "Guesses Left: " + left;
+
+        console.log("false");
+    }   
+
+    if (left == 0) {
+        var lossTally = document.getElementById("losses");
+            losses = y++;
+            lossTally.innerHTML = "Player Losses: " + losses;            
+    }
+
 }
-    // randomHero();
-
-
-console.log(document.getElementById("player").onkeyup = function() {playerGuess()})
-
-//letter is displayed along with previous chosen letters
-function playerGuess() {
-    document.getElementById("player").innerHTML = "playerGuess()";
-}
-
-console.log(playerGuess())
-
-//number of guesses is lowered if wrong number is inputted
-function noGuess(a, b) {
-    return guesses - 1;
-}
-
-console.log(noGuess);
-
-//computer score changes by +1 if player guesses reaches 0
-
-
-//player score changes if all letters in word are guessed
-
-
-//game resets
